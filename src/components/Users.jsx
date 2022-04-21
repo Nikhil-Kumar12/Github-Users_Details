91dc1f6c4f2bf2ff16287051532b0668938e0264
Nikhil Kumar <70438111+Nikhil-Kumar12@users.noreply.github.com>
2022-04-21 13:48:09 +0530

    fixes

--- src/components/Users.jsx
+++ src/components/Users.jsx
@@ -4,6 +4,7 @@ import { Link } from "react-router-dom/cjs/react-router-dom.min";
 
 const Users = () => {
   const [data, setData] = useState(null);
+  const [value, setValue] = useState("");
   useEffect(() => {
     axios.get("https://api.github.com/users").then((response) => {
       const api = response.data;
@@ -15,8 +16,26 @@ const Users = () => {
     <>
       {data ? (
         <div className="users">
-          <div className="heading">List of Users</div>
-            {data?.map((users) => {
+          <div className="top-main">
+            <div className="heading">List of Users</div>
+            <div className="search-box">
+              <input
+                type="text"
+                placeholder="Enter the value ..."
+                value={value}
+                onChange={(e) => setValue(e.target.value)}
+              />
+            </div>
+          </div>
+          {data
+            ?.filter((val) => {
+              if (value == "") {
+                return val;
+              } else {
+                return val.login.toLowerCase().includes(value.toLowerCase());
+              }
+            })
+            .map((users) => {
               return (
                 <div className="user-link" key={users.id}>
                   <Link className="link" to={`/${users.login}`} key={users.id}>
